37beb50073815f47428b132229882e891eead821
psuwebdevclub <133180219+psuwebdevclub@users.noreply.github.com>
2023-05-11 13:52:10 -0400

    fixed js error

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function changeTitle() {
 
 function test() {
     let object = document.getElementById("paragraphText");
-    console.print(object);
+    console.log(object);
 }
   changeTitle(); // Call the function to start the countdown
   
