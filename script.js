71346bc3370be2a39677e8d533cc80ee224d28ed
psuwebdevclub <133180219+psuwebdevclub@users.noreply.github.com>
2023-05-10 22:35:27 -0400

    final

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function changeTitle() {
   
 
 function test() {
-    let object = document.getElementByID("paragraphText");
+    let object = document.getElementById("paragraphText");
     console.out.println(object)
 }
   changeTitle(); // Call the function to start the countdown
